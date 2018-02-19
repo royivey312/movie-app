04b4429709bbea0c6c25cbc879a818b53db68535
Roy Ivey <royivey312@gmail.com>
2018-02-19 17:42:04 -0600

    Intial Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule       } from '@angular/platform-browser';
+import { NgModule            } from '@angular/core';
+import { FormsModule         } from '@angular/forms';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent        } from './app.component';
+import { MoviesComponent     } from './movies/movies.component';
+import { HeroDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieService        } from './movie.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import {AppRoutingModule} from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MovieService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

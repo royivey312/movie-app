33aca5d0c36c7698adf6bcd3b086228ef5e56117
Roy Ivey <royivey312@gmail.com>
2018-02-23 15:06:51 -0600

    Included movie-search componant and in memory data service

---
+++ src/app/movie-search/movie-search.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Movie} from '../movie';
+import {MovieService} from '../movie.service';
+import {Subject} from 'rxjs/Subject';
+import {switchMap, distinctUntilChanged, debounceTime} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-movie-search',
+  templateUrl: './movie-search.component.html',
+  styleUrls: ['./movie-search.component.css']
+})
+export class MovieSearchComponent implements OnInit {
+
+  movies$: Observable<Movie[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private movieService: MovieService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit() {
+    this.movies$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.movieService.searchMovies(term))
+    );
+  }
+
+}
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+class NavLink {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  navLinks: NavLink[] = [
+    {title: 'Home', link: '/dashboard'},
+    {title: 'My Movies', link: '/movies'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
